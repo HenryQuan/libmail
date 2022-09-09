89a835d8fab3f6312bfb07ad7db464a214476bf4
Yiheng <development.henryquan@gmail.com>
2022-09-09 21:37:24 +1000

    fixed c wrapper

--- c/main.c
+++ c/main.c
@@ -1,8 +1,13 @@
 #include "mail.h"
 #include <stdio.h>
 
-int main() {
-    int result = send_email("Hello", "Hello World", "");
+int main(int argc, char* argv[]) {
+    if (argc < 4) {
+        printf("Usage: mail <subject> <body> <to>");
+        return 0;
+    }
+    
+    int result = send_email(argv[1], argv[2], argv[3]);
     // true is returned
     return result == 1;
 }
