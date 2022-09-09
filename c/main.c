d742198f3f0230d3afa3999c110e0318e0e814ba
Yiheng <development.henryquan@gmail.com>
2022-09-09 22:13:59 +1000

    python

--- c/main.c
+++ c/main.c
@@ -4,7 +4,7 @@
 int main(int argc, char* argv[]) {
     if (argc < 4) {
         printf("Usage: mail <subject> <body> <to>");
-        return 0;
+        return -1;
     }
     
     int result = send_email(argv[1], argv[2], argv[3]);
